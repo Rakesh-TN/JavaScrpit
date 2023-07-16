d50abb982bd7221387bbdad8fc2d6a4c6c45cfb0
Rakesh-TN <tnrakesh68@outlook.com>
2023-07-16 12:57:27 +0530

    Task 04 Completed

---
+++ Variable Excercises/Task 04/task4.js
@@ -0,0 +1,6 @@
+var currentAge = 21
+var maxiAge = 60
+var estimatedage = maxiAge-currentAge
+var perDay = 145
+var manyTime = estimatedage * perDay
+console.log("You will need ", manyTime,"to last you until the ripe old age of", maxiAge)
