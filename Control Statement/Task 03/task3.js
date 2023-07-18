2498e8ddc50c76d549c9fb7f8095e027c8ae051c
Rakesh-TN <tnrakesh68@outlook.com>
2023-07-18 16:24:00 +0530

    Control statement Task 03 Completed

---
+++ Control Statement/Task 03/task3.js
@@ -0,0 +1,11 @@
+let age = prompt("Enter you are age")
+
+if (age > 18){
+    alert("You are able to create a account")
+}
+else if (age == 17){
+    alert("You can create a account with your family domain")
+}
+else if (age < 16){
+    alert("Your are not eligble to create a account")
+}
