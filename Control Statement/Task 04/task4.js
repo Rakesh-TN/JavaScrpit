e8732fb037f90125544d30dce6f459404a2a0901
Rakesh-TN <tnrakesh68@outlook.com>
2023-07-19 14:53:11 +0530

    Control Statement Almost Completed

---
+++ Control Statement/Task 04/task4.js
@@ -0,0 +1,11 @@
+for (let num = 1; num <=100; num++){
+    if (num % 3 == 0){
+        console.log("Number that divided by 3", num);
+    }
+    }
+    let num = 1
+    while (num <=100){
+    if (num % 5 == 0)
+     console.log("Number that divided by 5", num);
+        num++
+    }
---
+++ Control Statement/Task 05/task5.js
@@ -0,0 +1,11 @@
+for (let num = 1; num <=20;num++){
+    if (num % 3 == 0){
+        console.log(num, "Fizz");
+    }
+    else if (num % 5== 0){
+        console.log(num,"Buzz");
+    } 
+    else{
+        console.log(num)
+    }
+    }
