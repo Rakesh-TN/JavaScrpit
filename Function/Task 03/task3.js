d3edbdb221f277c5ce520e189d0cebf692def5ad
Rakesh-TN <tnrakesh68@outlook.com>
2023-07-20 14:41:53 +0530

    updating everthing

---
+++ Function/Task 03/task3.js
@@ -0,0 +1,5 @@
+function total(a,b) {
+    return a+b;
+  }
+  let result = total(200,10)
+  console.log(result)
---
+++ Function/Task 04/task4.js
@@ -0,0 +1,5 @@
+function average(a,b) {
+    return (a+b)/2;
+  }
+  let result = average(4,2)
+  console.log(result)
