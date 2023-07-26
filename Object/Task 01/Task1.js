3d148c8c161fa1ae0239d17a28524a9aa6395909
Rakesh-TN <tnrakesh68@outlook.com>
2023-07-26 15:34:31 +0530

    Object Complteled

---
+++ Object/Task 01/Task1.js
@@ -0,0 +1,8 @@
+let car = {
+    name: "Mercedes Benz",
+    model: "C-Class",
+    manufacture: "Daimler AG Company",
+    length: "4686mm",
+    cargocapacity: "12.6 cubic feet",
+  }
+  console.log(car)
